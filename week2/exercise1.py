68bde504ba199186a8f4069df00824be9464088c
zhiyuan Lu <owenlu0321@gmail.com>
2020-06-10 23:08:01 +1000

    week2 1

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -11,19 +11,25 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
+# It creates a list with string-variables
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
 
+# Select each variables and print them seprately within the terminal.
 for word in some_words:
     print(word)
 
+# it does the same action as the command above. The point is variables could be called as any names in the list.
 for x in some_words:
     print(x)
 
+# Print the list within the terminal.
 print(some_words)
 
+# It creates the statement of the length of the list is greater than 3. The boolean answer is true and it operates the next line which is printing the string in the terminal.
 if len(some_words) > 3:
     print('some_words contains more than 3 words')
 
+# Uname is used to get information from the current device operating system. The print command is able to display the info within the terminal.
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
