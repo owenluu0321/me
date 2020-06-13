d9ba6b71d0e2519eeb0898fbc979b507fff78f5f
zhiyuan Lu <owenlu0321@gmail.com>
2020-06-13 17:17:31 +1000

    week 2 final

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -1,5 +1,4 @@
 """Correct the syntax in this file.
-
 This file doesn't run yet.
 Go through it and change it until it runs.
 Remeber that all files must also pass the
@@ -8,21 +7,21 @@ linter with no errors or warnings!
 
 import string
 
-det getLetter(index):
+def getLetter(index):
     alphabet = string.ascii_lowercase + " "
-    return alphabet(index]
+    return alphabet[index]
 
 
-def week2exersise2();
-    indices = [12: 2, 26, 7, 0, 12, 12, 4, 17]
+def week2exersise2():
+    indices = [12, 2, 26, 7, 0, 12, 12, 4, 17]
     wordArray = map(getLetter, indices)
     wordArray[0] = wordArray[0].upper()
-    wordArray{1} = wordArray[1].upper()
-    wordArray[3} = wordArray[3].upper{}
-    secret_word="".join(wordArray)
+    wordArray[1] = wordArray[1].upper()
+    wordArray[3] = wordArray[3].upper()
+    secret_word = "".join(wordArray)
     print(secret_word)
     return secret_word
 
 
-if __name__ = = "__main__":
-    prin(week2exersise2())
+if __name__ == "__main__":
+    print(week2exersise2())
--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,7 +7,10 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    return None
+    if a_number % 2 ==0:
+        return False
+    elif a_number % 2 != 0:
+        return True
 
 
 def fix_it(moves=True, should_move=True):
@@ -25,7 +28,14 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return None
+    if moves == True and should_move == True:
+        return "No Problem"
+    if moves == True and should_move == False:
+        return "Duct Tape"
+    if moves == False and should_move == True:
+        return "WD-40"
+    if moves == False and should_move == False:
+        return "No Problem" 
 
 
 def loops_1a():
@@ -35,7 +45,8 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    string_list_a = ['*'] * 10
+    return string_list_a
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -45,7 +56,8 @@ def loops_1c(number_of_items=5, symbol="#"):
     string with exacly one symbol in it.
     E.g.: ['#', '#', '#', '#', '#']
     """
-    return None
+    string_list_b = [symbol] * number_of_items
+    return string_list_b
 
 
 def loops_2():
@@ -66,7 +78,9 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+    string_list_a = ['*'] * 10
+    string_list_c = [string_list_a] * 10
+    return string_list_c
 
 
 def loops_3():
@@ -90,7 +104,13 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    lista = []
+    for i in range(0, 10):
+        listb = []
+        for ii in range(10):
+            listb.append(str(i))
+        lista.append(listb)
+    return lista
 
 
 def loops_4():
@@ -110,7 +130,13 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    return None
+    list1 = []
+    for a in range(10):
+        list2 = []
+        for aa in range(10):
+            list2.append(str(aa))
+        list1.append(list2)
+    return list1
 
 
 def loops_5():
@@ -137,7 +163,13 @@ def loops_5():
         "There are {} green bottles".format(8)
     you'll come to see the pros and cons of each over time.
     """
-    return None
+    lista = []
+    for a in range(10):
+        listb = []
+        for aa in range(5):
+           listb.append("(" + "i" + str(a) + "," + " " + "j" + str(aa) + ")")
+        lista.append(listb)
+    return lista
 
 
 def loops_6():
@@ -160,7 +192,13 @@ def loops_6():
     You can use a variable.
     TIP: look out for the starting condition.
     """
-    return None
+    list1 = []
+    for i in range(10):
+        list2 = []
+        for ii in range(i + 1):
+            list2.append(str(ii))
+        list1.append(list2)
+    return list1
 
 
 def loops_7():
@@ -184,8 +222,18 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    return None
-
+    row = []
+    for i in range(1, 6):
+        column = []
+        for ii in range(0, 5 - i):
+            column.append(" ")
+        for ii in range(6 - i, ((5 - i + 1) + 2 * i) - 1):
+            column.append("*")
+        for ii in range(((5 - i + 1) + 2 * i), 11):
+            column.append(" ")   
+        row.append(column)
+    return row
+            
 
 def lp(some_kind_of_list, exercise_name):
     """Help to see what's going on.
